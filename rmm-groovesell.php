3d1321b0788fd4c37221b2d668a9dc0357524ee2
Toronto SEO Agency | Rapid MAP Media <adrianyh@rapidmapmedia.com>
2020-08-24 23:18:32 -0400

    Added rmm-groovesell.php file
    Test file to explode JSON values via webhook

---
+++ rmm-groovesell.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Content-Type: application/json');
+$request = explode('&', file_get_contents('php://input'));
+
+
+foreach ($request as $pair) {
+    $part = explode('=', $pair);
+    $str .= $part[0] . ": " . urldecode($part[1]) . "\n";
+    /*
+    if ($part[0] == "buyer_email") {
+        $strEmail = urldecode($part[1]);
+    break;
+    }
+    */
+}
+
+
+//$req_dump = print_r( $strEmail . "\n\n" . $result, true );
+
+$req_dump = print_r( $str . "\n\n" , true );
+$fp = file_put_contents( 'request.log', $req_dump );
+
+?>
